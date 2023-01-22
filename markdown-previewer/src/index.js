b30ef97c1b59b5b7952ec0696cf46d13fdb5e0e9
Abhinav <abhi.gupta8802@gmail.com>
2023-01-22 13:36:38 +0530

    mardown previewer initial

---
+++ markdown-previewer/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'index.css';
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+    }
+}
+
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
