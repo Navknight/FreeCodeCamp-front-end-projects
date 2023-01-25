aab74266b6eee6d76a2395be0b32ade55ae925b9
Abhinav <abhi.gupta8802@gmail.com>
2023-01-25 22:54:05 +0530

    complete

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,13 +1,22 @@
-import { useState } from 'react';
-import './App.css';
-import { marked } from 'marked';
+import { useState } from "react";
+import "./App.css";
+import { marked } from "marked";
 
 function App() {
-  const [text, setText] = useState('## hello');
-  return (  
+  const [text, setText] = useState("## hello");
+  return (
     <div className="App">
-      <textarea id="editor" value={text} onChange={(event)=>{setText(event.target.value)}}></textarea>
-      <div id="preview" dangerouslySetInnerHTML={{__html: marked(text)}}></div>
+      <textarea
+        id="editor"
+        value={text}
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+      ></textarea>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{ __html: marked(text) }}
+      ></div>
     </div>
   );
 }
